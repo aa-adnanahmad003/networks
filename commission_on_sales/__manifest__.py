6abadb8f96d6ac4ad0b1dc27e9415c0e78950896
Farooq Butt <farooqbutt548@gmail.com>
2024-01-21 18:45:54 +0500

    [MOD] commission on sale module reports and commission generate

--- commission_on_sales/__manifest__.py
+++ commission_on_sales/__manifest__.py
@@ -12,14 +12,18 @@
     'version': '16.0.1.0.0',
     'license': 'LGPL-3',
     # Dependencies
-    'depends': ['base','contacts'],
+    'depends': ['base', 'sale_subscription'],
 
     # Views
     'data': [
         'security/ir.model.access.csv',
+        'data/schedual_actions.xml',
         'views/commission_agent.xml',
         'views/res_partner_ext.xml',
-        'views/commission_settlement.xml',
+        'views/sale_order_ext.xml',
+        'views/commission_table.xml',
+        'wizards/commission_wizard.xml',
+        'reports/commission_calculate_report.xml',
     ],
 
     # Technical
--- commission_on_sales/models/commission_settlement.py
+++ commission_on_sales/models/commission_settlement.py
@@ -17,7 +17,7 @@ class CommissionSettlementLine(models.Model):
     _name = 'commission.settlement.line'
     _description = 'Commission Settlement Line'
 
-    partner_id = fields.Many2one('res.partner', string='Customer', domain=[('commission_agent_id', '!=', False)])
+    partner_id = fields.Many2one('res.partner', string='Customers', domain=[('commission_agent_id', '!=', False)])
     fixed_amount = fields.Float(string='Fixed Amount', default=0.0)
     commission_settlement_id = fields.Many2one('commission.settlement', string='Commission Settlement')
 
