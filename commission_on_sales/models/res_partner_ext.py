a241fe2a74bcd1b7def5b24d8a452e9279818604
Farooq Butt <farooqbutt548@gmail.com>
2024-02-06 21:26:32 +0500

    [FIX] reset rec name of res partner

--- commission_on_sales/models/res_partner_ext.py
+++ commission_on_sales/models/res_partner_ext.py
@@ -5,9 +5,9 @@ class ResPartnerExt(models.Model):
 
     commission_agent_id = fields.Many2one('commission.agent', string='Commissioned Agent', ondelete='restrict')
 
-    def name_get(self):
-        result = []
-        for rec in self:
-            name = f"{rec.name} - ({rec.commission_agent_id.user_id.name})"
-            result.append((rec.id, name))
-        return result
+    # def name_get(self):
+    #     result = []
+    #     for rec in self:
+    #         name = f"{rec.name} - ({rec.commission_agent_id.user_id.name})"
+    #         result.append((rec.id, name))
+    #     return result
